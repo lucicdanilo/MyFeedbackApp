c0584222593f00b8a5be53d3e874a841fcabff77
Lucic Danilo <lucic.danilo94@gmail.com>
2020-01-06 21:18:08 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+var categories = ["Projects", "Website", "CV"];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 class="title">Wellcome to my Feedback App</h1>
+      <h4 class="subtitle">
+        I build this app for learning React, improving my old projects and
+        finding ideas for future projects as well!
+      </h4>
+      <h2>{categories.length}</h2>
     </div>
   );
 }
