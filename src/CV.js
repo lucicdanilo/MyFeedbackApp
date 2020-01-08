7586db9ea1caae29abc456c6ad667730da862dc0
Lucic Danilo <lucic.danilo94@gmail.com>
2020-01-08 01:05:46 +0100

    styling

--- src/CV.js
+++ src/CV.js
@@ -3,9 +3,29 @@ import React, { Component } from "react";
 class CV extends Component {
   render() {
     return (
-      <div>
-        <h2>Feedback for CV</h2>
-      </div>
+      <form>
+        <div class="boxRate">
+          <h2 class="dynamicSubtitle">Feedback for CV:</h2>
+          <div class="designRate">
+            <p class="titleForSlider">Ocijeni dizajn CV-a:</p>
+            <div class="slidecontainer">
+              <input type="range" min="0" max="10" step="1" />
+            </div>
+          </div>
+          <div class="contentRate">
+            <p class="titleForSlider">Ocijeni sadrzaj CV-a:</p>
+            <div class="slidecontainer">
+              <input type="range" min="0" max="10" step="1" />
+            </div>
+          </div>
+          <div calss="textSubmitReview">
+            <textarea rows="6" cols="50">
+              Here you can send me ideas for better CV on design and content.
+            </textarea>
+          </div>
+          <input type="submit" class="submitButton" value="Submit" />
+        </div>
+      </form>
     );
   }
 }
--- src/Website.js
+++ src/Website.js
@@ -3,9 +3,36 @@ import React, { Component } from "react";
 class Website extends Component {
   render() {
     return (
-      <div>
-        <h2>Feedback for Website</h2>
-      </div>
+      <form>
+        <div class="boxRate">
+          <h2 class="dynamicSubtitle">Feedback for Website:</h2>
+          <div class="designRate">
+            <p class="titleForSlider">Ocijeni dizajn sajta:</p>
+            <div class="slidecontainer">
+              <input type="range" min="0" max="10" step="1" />
+            </div>
+          </div>
+          <div class="performanceRate">
+            <p class="titleForSlider">Ocijena performansi:</p>
+            <div class="slidecontainer">
+              <input type="range" min="0" max="10" step="1" />
+            </div>
+          </div>
+          <div class="contentRate">
+            <p class="titleForSlider">Ocijeni sadrzaj sajta:</p>
+            <div class="slidecontainer">
+              <input type="range" min="0" max="10" step="1" />
+            </div>
+          </div>
+          <div calss="textSubmitReview">
+            <textarea rows="6" cols="50">
+              Here you can send me ideas for better website on design,
+              performance, content and etc.
+            </textarea>
+          </div>
+          <input type="submit" class="submitButton" value="Submit" />
+        </div>
+      </form>
     );
   }
 }
