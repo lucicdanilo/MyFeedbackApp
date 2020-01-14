b2f91a21f1f38dd9c7299abf3034dd0e09347ebd
Lucic Danilo <lucic.danilo94@gmail.com>
2020-01-14 12:09:56 +0100

    finished

---
+++ feedbackApp/precache-manifest.8425332db7d321f488986d94fd293cbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a4a2a92acf860cccaef6e0f442b9b72",
+    "url": "/index.html"
+  },
+  {
+    "revision": "279d6e17d9f1d3a9781d",
+    "url": "/static/css/main.4199e2b6.chunk.css"
+  },
+  {
+    "revision": "f962b4bb58eb58702a06",
+    "url": "/static/js/2.b2318156.chunk.js"
+  },
+  {
+    "revision": "f5ca32966347038a998bbf09dda684c7",
+    "url": "/static/js/2.b2318156.chunk.js.LICENSE"
+  },
+  {
+    "revision": "279d6e17d9f1d3a9781d",
+    "url": "/static/js/main.441866ae.chunk.js"
+  },
+  {
+    "revision": "0c466ab1961aac4a4d38",
+    "url": "/static/js/runtime-main.41c22845.js"
+  }
+]);
